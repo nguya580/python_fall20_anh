a568444176e3d564bdf715a9916d7da895f6d24d
Anh Nguyen <nguya580@newschool.edu>
2020-11-13 13:28:00 -0500

    graph update for exercise 3

--- week_03/week03_submission/week03_scrapbook.py
+++ week_03/week03_submission/week03_scrapbook.py
@@ -98,3 +98,41 @@ plt.xticks(range(len(data)), list(data.keys()), rotation = 45,)
 plt.suptitle('Most used apps screen time of Anh')
 
 plt.show()
+
+
+
+
+# %% codecell
+import matplotlib.pyplot as plt
+
+data_anh = {'Youtube': 6, 'Instagram': 3, 'Facebook': 2, 'Chrome': 1}
+data_teddy = {'Youtube': 10, 'Instagram': 6, 'Facebook': 6, 'Chrome': 3}
+
+app_names_y = list(data_anh.keys())
+values_anh_x = list(data_anh.values())
+value_teddy_x = list(data_teddy.values())
+
+plt.plot(app_names_y, values_anh_x, linestyle='--', label='Anh data', marker='s')
+plt.plot(app_names_y, value_teddy_x, label='Teddy data', marker='s')
+
+plt.xlabel('App Names')
+plt.ylabel('Hours')
+plt.title('Daily screen time of Anh and Teddy')
+
+#set color for every legend text with the same color as lines in figure
+legend_style = plt.legend()
+for line, text in zip(legend_style.get_lines() ,legend_style.get_texts()):
+    text.set_color(line.get_color())
+
+plt.grid(True)
+
+# print(plt.style.available)
+#['seaborn-dark', 'seaborn-darkgrid', 'seaborn-ticks', 'fivethirtyeight', 'seaborn-whitegrid', 'classic', '_classic_test', 'fast', 'seaborn-talk', 'seaborn-dark-palette', 'seaborn-bright', 'seaborn-pastel', 'grayscale', 'seaborn-notebook', 'ggplot', 'seaborn-colorblind', 'seaborn-muted', 'seaborn', 'Solarize_Light2', 'seaborn-paper', 'bmh', 'tableau-colorblind10', 'seaborn-white', 'dark_background', 'seaborn-poster', 'seaborn-deep']
+plt.style.use('seaborn-ticks')
+
+plt.tight_layout()
+
+#uncomment this line to save graph as svg file
+# plt.savefig("anh_teddy_dailyscreentime.svg", format="svg")
+
+plt.show()
